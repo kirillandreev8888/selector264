be18da722752d8f5dad4a9271c885daddda8b3f6
Kirill <kirillandreev8888@gmail.com>
2020-09-04 15:43:37 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Helmet } from "react-helmet"
+//bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Nav } from "react-bootstrap"
+//components
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const titles = ["애니메이션", "アニメ", "はさぎ", "하사기"];
+	function randomInteger(min, max) {
+		let rand = min - 0.5 + Math.random() * (max - min + 1);
+		return Math.round(rand);
+	}
+
+	return (
+		<div className="App">
+			<Helmet>
+				<title>{titles[randomInteger(0,titles.length)]}</title>
+			</Helmet>
+		</div>
+	);
 }
 
 export default App;
