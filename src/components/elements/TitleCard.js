a4e3ac6e3bd8c0081f9e9622a947c304525c4ddc
Kirill <kirillandreev8888@gmail.com>
2020-09-12 16:15:40 +0300

    Minmode доделан

--- src/components/elements/TitleCard.js
+++ src/components/elements/TitleCard.js
@@ -3,6 +3,8 @@ import { Card, Button, Accordion } from 'react-bootstrap'
 import "./TitleCard.scss"
 import { Link } from 'react-router-dom'
 
+import icon_edit from "../../static/media/icon_edit.svg"
+
 function TitleCard(props) {
     return (
         <div className="TitleCard">
@@ -10,13 +12,23 @@ function TitleCard(props) {
                 if (props.minmode === "Min")
                     return (
                         <Accordion defaultActiveKey="1">
-                            <Accordion.Toggle as={Card.Header} eventKey="0"><h3>{props.name}</h3></Accordion.Toggle>
+                            <Accordion.Toggle
+                                style={{ display: "flex", justifyContent: "center" }}
+                                as={Card.Header} eventKey="0">
+                                <h3 style={{ margin: "auto" }}>
+                                    {props.name}
+                                </h3>
+                                {/* <Link to={{ pathname: `edit`, search: `?key=${props.id}&from=${props.path}` }}>
+                                    <img style={{ height: "50px" }} src={icon_edit} alt="редактировать" /></Link> */}
+
+                            </Accordion.Toggle>
                             <Accordion.Collapse eventKey="0">
                                 <Card >
                                     <Card.Img className="animate1" width="100%" src={props.pic} alt="Нет картинки" />
                                     <Card.Body>
-                                        <Card.Title ><Link to={{ pathname: `edit`, search: `?key=${props.id}&from=${props.path}` }}>{props.name}</Link></Card.Title>
                                         <div>
+                                        <Button variant="warning" className="w-50"  >Редактировать</Button>
+
                                             {(() => {
                                                 if (props.shiki_link !== "")
                                                     return (<Button variant="primary" className="w-50" href={props.shiki_link} >Страница на shikimori.one</Button>)
@@ -25,12 +37,10 @@ function TitleCard(props) {
                                             })()}
                                             {(() => {
                                                 if (props.watch_link !== "")
-                                                    return (<Button variant="success" className="w-25" href={props.watch_link} style={{ marginLeft: "20px" }}>Смотреть</Button>)
+                                                    return (<Button variant="success" className="w-50" href={props.watch_link} >Смотреть</Button>)
                                                 else
-                                                    return (<Button disabled variant="outline-success" className="w-25" href={props.watch_link} style={{ marginLeft: "20px" }}>Смотреть</Button>)
+                                                    return (<Button disabled variant="outline-success" className="w-50" href={props.watch_link} style={{ marginLeft: "20px" }}>Смотреть</Button>)
                                             })()}
-
-
                                         </div>
                                     </Card.Body>
                                 </Card>
